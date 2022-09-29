d0d8740b27eaa334bc3a74896fc1f06b365abb0e
Hudson Wesel <cswyld7k@gmail.com>
2022-09-29 02:57:32 -0400

    Game Functionality

--- src/Madlibs.java
+++ src/Madlibs.java
@@ -1,58 +1,88 @@
+
 /**
  * @author Hudson Wesel
  * GameEngine for MadLibs
  */
 
 import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 import java.lang.Thread;
 
 public class Madlibs {
-    
-    // private boolean isPlaying = false;
-    // Creates scanner object
-    private Scanner scanner = new Scanner(System.in); 
-    private String answer;
+    // ========VARIABLES========
+    private String story;
+    private String userAnswer;
 
+    private Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<String> answers = new ArrayList<String>();
+
+    // ========METHODS========
+
+    // ========CONSTRUCTOR========
     /**
+     * Madlibs constructor method
      * 
-     * @param question
-     * @return <String> Question asked to user
+     * @param story
      */
-    
-    
-    public String askQuestion(String question){
-        System.out.print(question);
-        answer = scanner.nextLine();
 
-        return "InputtedAnswer"; // Use Scanner to get answer
+    public Madlibs(String story) {
+        this.story = story;
     }
 
-
-
-
+    // =====askQuestion method=====
 
     /**
-     * Replaces filler words with inputtedAnswer
-     * @param inputtedAnswer
-     * @return <String> story
+     * Postcondition: questions user, recieves input from user, calls user method
+     * 
+     * @param {question} //Question asked to user
      */
 
-     public void addWord(String inputtedAnswer) {
-        
-
-     }
-
-    public String writeStory(){
-        // Find the place where answer should be inputted 
-        // Return story
-        return "THE STORY";
+    public void askQuestion(String question) {
+        System.out.print(question);
+        userAnswer = scanner.nextLine();
+        addAnswer(userAnswer);
     }
 
-    public void sleepThread(){
+    /**
+     * =====addAnswer Method=====
+     * Postcondition: adds inputted answer from askQuestion to ArrayList answers
+     * 
+     * @param {inputtedAnswer} answer from user in askQuestion method
+     */
+
+    private void addAnswer(String inputtedAnswer) {
+        answers.add(userAnswer);
+    }
+
+    /**
+     * =====printAnswers Method=====
+     * Postcondition: Prints all inputted answers from user
+     */
+
+    public void printAnswers() {
+        System.out.println("Your Answers Were: " + answers);
+    }
+
+    public void printStory() {
+        for (String answer : answers) {
+            story = story.replace("FILLER", answer);
+        }
+
+        System.out.println(story);
+    }
+
+    /**
+     * =====sleepThread Method=====
+     * Postcondition: Pauses threads from processing code for 4 seconds
+     */
+
+    public void sleepThread(int time) {
+        // Thread.sleep() must be wrapped in try/catch to run without error
         try {
-            Thread.sleep(4000);            
-        } catch (InterruptedException e  ) {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
             System.out.println("ERROR: Thread Interuption Quitting...");
 
         }
@@ -60,16 +90,27 @@ public class Madlibs {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to MadLibs...."); 
+        String story1 = "FILLER is on a quest FILLER their buddies and needs your help!" +
+                "Your cautious so will bring a FILLER to help you on your quest!" +
+                "You begin your search at FILLER";
 
+        Madlibs game = new Madlibs(story1);
 
-            
-        }
+        System.out.println("Welcome to MadLibs & Prepare For Adventure!");
+        game.sleepThread(500);
+        System.out.println("Starting Game...");
+        game.sleepThread(1000);
+
+        // =======MADLIBS QUESTIONS=======
+        game.askQuestion("Please Give Me A Name 📇: ");
+        game.askQuestion("Please Give Me An Infinitive formatted (to VERB)✏️: ");
+
+        game.askQuestion("Please Give Me A Noun ❇️: ");
+        game.askQuestion("Please Give Me A Place 🏝: ");
+
+        game.printStory();
+
+        game.printAnswers();
+
     }
-    
-
-
-
-
-    
-
+}
